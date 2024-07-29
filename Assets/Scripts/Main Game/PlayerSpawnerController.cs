3e70bb7638dc7a24aa5c75497b3dd89dded5fd28
Ashutosh <ashunandanwar98@gmail.com>
2024-07-29 14:32:58 +0530

    Player Joined and Left

--- Assets/Scripts/Main Game/PlayerSpawnerController.cs
+++ Assets/Scripts/Main Game/PlayerSpawnerController.cs
@@ -3,22 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawnerController : NetworkBehaviour, IPlayerJoined
+public class PlayerSpawnerController : NetworkBehaviour, IPlayerJoined, IPlayerLeft
 {
     [SerializeField] private NetworkPrefabRef playerNetworkPrefab = NetworkPrefabRef.Empty;
     [SerializeField] private Transform[] spawnPoints;
 
-
+    public override void Spawned()
+    {
+        if(Runner.IsServer)
+        {
+            foreach(var item in Runner.ActivePlayers)
+            {
+                SpawnPlayer(item);
+            }
+        }
+    }
     private void SpawnPlayer(PlayerRef playerRef)
     {
         if(Runner.IsServer)
         {
-            Runner.Spawn(playerNetworkPrefab, Vector3.zero, Quaternion.identity, playerRef);
+            var index = playerRef % spawnPoints.Length;
+            var spawnPoint = spawnPoints[index].transform.position;
+            var playerObj = Runner.Spawn(playerNetworkPrefab, spawnPoint, Quaternion.identity, playerRef);
+
+            Runner.SetPlayerObject(playerRef, playerObj);
         }
     }
+
+    private void DespawnPlayer(PlayerRef playerRef)
+    {
+        if(Runner.IsServer)
+        {
+            if(Runner.TryGetPlayerObject(playerRef, out var playerNetworkObj))
+            {
+                Runner.Despawn(playerNetworkObj);
+            }
+
+            Runner.SetPlayerObject(playerRef, null);
+        }
+    }
+
     public void PlayerJoined(PlayerRef player)
     {
         SpawnPlayer(player);
     }
 
+    public void PlayerLeft(PlayerRef player)
+    {
+        DespawnPlayer(player);
+    }
 }
